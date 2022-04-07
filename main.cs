8005cec04dbdab7fa40d28104b77948f310cc410
jaejun-lee06 <99355321+jaejun-lee06@users.noreply.github.com>
2022-04-07 03:20:33 +0000

    done

--- main.cs
+++ main.cs
@@ -1,7 +1,25 @@
+// Created by: Jaejun Lee
+// Created on: Mar 2022
+//
+// This program accepts user input
 using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        double radius;
+        double volume;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+        Console.WriteLine("This program calculates the volume of a sphere.");
+        Console.WriteLine("");
+        Console.Write("Enter the radius (cm): ");
+        radius = double.Parse(Console.ReadLine());
+
+        volume = (double)4 / 3 * Math.PI * Math.Pow(radius, 3);
+
+        Console.WriteLine("");
+        Console.Write("The volume of the sphere = " + volume.ToString("0.00") + " cm²");
+        Console.ReadKey();
+        Console.WriteLine("\nDone.");
+    }
 }
